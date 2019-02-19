4e6ddfe5a44381df7c8a1b1a26f91d1e61b08acb
Otto Hyytiälä <otto.hyytiala@gmail.com>
2019-02-19 12:14:15 +0200

    buttons fix

--- src/components/CountForm.js
+++ src/components/CountForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
+import {withStyles} from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
@@ -50,7 +50,13 @@ class Key extends React.Component {
     const {id, className, onPress} = this.props
     return (
       <PointTarget onPoint={onPress}>
-        <Button className={className} onClick={onPress}>{id}</Button>
+        <Button
+          disableRipple={true}
+          disableFocusRipple={true}
+          className={className}
+          onClick={onPress}>
+          {id}
+        </Button>
       </PointTarget>
     )
   }
@@ -65,20 +71,20 @@ class countForm extends React.Component {
   }
 
   handleInput = (digit) => {
-    this.setState({ value: this.state.value === '0' ? digit : this.state.value + digit })
+    this.setState({value: this.state.value === '0' ? digit : this.state.value + digit})
   }
 
   handleInputChange = (event) => {
-    this.setState({ value: event.target.value })
+    this.setState({value: event.target.value})
   }
 
   handleSend = (event) => {
     this.props.handleCount(this.state.value)
-    this.setState({ value: '0' })
+    this.setState({value: '0'})
   }
 
   resetInput = (event) => {
-    this.setState({ value: '0' })
+    this.setState({value: '0'})
   }
 
   render(props) {
@@ -89,68 +95,68 @@ class countForm extends React.Component {
           {this.props.title}
         </Typography>
         <div className={classes.root}>
-        <TextField
-          id="standard-number"
-          type="text"
-          className={classes.textField}
-          InputProps={{
-            classes: {
-              input: classes.input,
-            },
-          }}
-          value={this.state.value}
-          onChange={this.handleInputChange}
-          margin="normal"
-          variant="outlined"
-          disabled
-        />
-        <Grid container spacing={8}>
-          <Grid container item xs={12} spacing={8}>
-            <Grid item xs={4}>
-              <Key id='1' className={classes.button} onPress={() => this.handleInput('1')}/>
+          <TextField
+            id="standard-number"
+            type="text"
+            className={classes.textField}
+            InputProps={{
+              classes: {
+                input: classes.input,
+              },
+            }}
+            value={this.state.value}
+            onChange={this.handleInputChange}
+            margin="normal"
+            variant="outlined"
+            disabled
+          />
+          <Grid container spacing={8}>
+            <Grid container item xs={12} spacing={8}>
+              <Grid item xs={4}>
+                <Key id='1' className={classes.button} onPress={() => this.handleInput('1')}/>
+              </Grid>
+              <Grid item xs={4}>
+                <Key id='2' className={classes.button} onPress={() => this.handleInput('2')}/>
+              </Grid>
+              <Grid item xs={4}>
+                <Key id='3' className={classes.button} onPress={() => this.handleInput('3')}/>
+              </Grid>
             </Grid>
-            <Grid item xs={4}>
-              <Key id='2' className={classes.button} onPress={() => this.handleInput('2')}/>
+            <Grid container item xs={12} spacing={8}>
+              <Grid item xs={4}>
+                <Key id='4' className={classes.button} onPress={() => this.handleInput('4')}/>
+              </Grid>
+              <Grid item xs={4}>
+                <Key id='5' className={classes.button} onPress={() => this.handleInput('5')}/>
+              </Grid>
+              <Grid item xs={4}>
+                <Key id='6' className={classes.button} onPress={() => this.handleInput('6')}/>
+              </Grid>
             </Grid>
-            <Grid item xs={4}>
-              <Key id='3' className={classes.button} onPress={() => this.handleInput('3')}/>
+            <Grid container item xs={12} spacing={8}>
+              <Grid item xs={4}>
+                <Key id='7' className={classes.button} onPress={() => this.handleInput('7')}/>
+              </Grid>
+              <Grid item xs={4}>
+                <Key id='8' className={classes.button} onPress={() => this.handleInput('8')}/>
+              </Grid>
+              <Grid item xs={4}>
+                <Key id='9' className={classes.button} onPress={() => this.handleInput('9')}/>
+              </Grid>
+            </Grid>
+            <Grid container item xs={12} spacing={8}>
+              <Grid item xs={4}>
+                <Key id='Reset' className={classes.button} onPress={() => this.resetInput()}/>
+              </Grid>
+              <Grid item xs={4}>
+                <Key id='0' className={classes.button} onPress={() => this.handleInput('0')}/>
+              </Grid>
+              <Grid item xs={4}>
+                <Key id='OK' className={classes.button} onPress={() => this.handleSend()}/>
+              </Grid>
             </Grid>
           </Grid>
-          <Grid container item xs={12} spacing={8}>
-            <Grid item xs={4}>
-              <Key id='4' className={classes.button} onPress={() => this.handleInput('4')}/>
-            </Grid>
-            <Grid item xs={4}>
-              <Key id='5' className={classes.button} onPress={() => this.handleInput('5')}/>
-            </Grid>
-            <Grid item xs={4}>
-              <Key id='6' className={classes.button} onPress={() => this.handleInput('6')}/>
-            </Grid>
-          </Grid>
-          <Grid container item xs={12} spacing={8}>
-            <Grid item xs={4}>
-              <Key id='7' className={classes.button} onPress={() => this.handleInput('7')}/>
-            </Grid>
-            <Grid item xs={4}>
-              <Key id='8' className={classes.button} onPress={() => this.handleInput('8')}/>
-            </Grid>
-            <Grid item xs={4}>
-              <Key id='9' className={classes.button} onPress={() => this.handleInput('9')}/>
-            </Grid>
-          </Grid>
-          <Grid container item xs={12} spacing={8}>
-            <Grid item xs={4}>
-              <Key id='Reset' className={classes.button} onPress={() => this.resetInput()}/>
-            </Grid>
-            <Grid item xs={4}>
-              <Key id='0' className={classes.button} onPress={() => this.handleInput('0')}/>
-            </Grid>
-            <Grid item xs={4}>
-              <Key id='OK' className={classes.button} onPress={() => this.handleSend()}/>
-            </Grid>
-          </Grid>
-        </Grid>
-      </div>
+        </div>
       </div>
     )
   }
--- src/services/usages.js
+++ src/services/usages.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/usages'
+//const baseUrl = '/api/usages'
 
 
 const create = async (newObject) => {
